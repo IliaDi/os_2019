99b4d7a684dde64f68da31c850bb60d37512ccf9
IliaDi <ilia_dioleti@hotmail.com>
2019-03-27 19:16:52 +0200

    kinda works. but not

--- test3.c
+++ test3.c
@@ -6,7 +6,8 @@
 #include<sys/wait.h>
 
 int main(int argc, char **argv) {
-    int status, l, temp;
+    int status,  temp;
+	int l =1;
     int res = 1;
     int n = atoi(argv[1]);
     int k = atoi(argv[2]);
@@ -30,9 +31,7 @@ int main(int argc, char **argv) {
 
             // Parent process
         else if (c[i] > 0) {
-            printf("parent\n");
             close(fd1[0][0]);
-            l = 1;
             strcpy(input_str, "1");
 
             write(fd1[0][1], input_str, strlen(input_str) + 1);
@@ -43,48 +42,37 @@ int main(int argc, char **argv) {
 
             // child process
         else {
-            printf("this is child %d \n", i);
-            if (l <= k) {
-                if (i == 1) {
-                    if (l == 1) {
-                        close(fd1[i][1]);
-                        char my_str[100];
-                        read(fd1[i][0], my_str, 100);
-                        temp = atoi(my_str); //string->int
-                        res = temp * l;
-                        close(fd1[i][0]);
-
-                    } else {
+            while (l <= k) {
+                     printf("This is child %d , l is %d \n",i,l);
+                     if(i==1 && l!=1){ 
                         close(fd1[n + 1][1]);
                         char my_str[100];
                         read(fd1[n + 1][0], my_str, 100);
                         temp = atoi(my_str); //string->int
+						printf("This is temp (the int from the read string) %d \n",temp);
                         res = temp * l;
                         close(fd1[n + 1][0]);
                     }
+					else {
+                        close(fd1[i][1]);
+                        char my_str[100];
+                        read(fd1[i][0], my_str, 100);
+                        temp = atoi(my_str); //string->int
+						printf("This is temp (the int from the read string) %d \n",temp);
+                        res = temp * l;
+                        close(fd1[i][0]);
+
+                    }
                     l++;
                     close(fd1[i][0]);
+					printf("the res as an int is %d \n", res);
                     sprintf(input_str, "%d", res);
-                    //strcpy(input_str, "1");
+					printf("Now the res as a string is %s \n", input_str);
                     write(fd1[i + 1][1], input_str, strlen(input_str) + 1);
                     close(fd1[i + 1][1]);
-                } else {
-                    close(fd1[i][1]);
-
-                    char my_str[100];
-                    read(fd1[i][0], my_str, 100);
-
-                    printf("%s", my_str);
-                    close(fd1[i][0]);
-                    strcpy(input_str, "1");
-
-                    write(fd1[i + 1][1], input_str, strlen(input_str) + 1);
-                    close(fd1[i + 1][1]);
-                }
-
-
-                exit(0);
+              
             }
+			exit(0);
         }
     }
 } 
